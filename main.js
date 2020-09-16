ea98979a145be9235e1d33eebf35508e7eda37f2
seunnnc <seunnnc@users.noreply.github.com>
2020-09-16 23:55:27 +0900

    실습

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 var http = require('http');
 var fs = require('fs');
+var url = http.request('url')
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id)
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
+    if(_url == '/favicon.ico'){
+        return response.writeHead(404);
     }
-    response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync('__dirname' + url));
+    response.writeHead(200)
+    response.end(queryData.id);
  
 });
 app.listen(3000);
+
+// 터미널 실행 방법 => cd ~/dev/nodejs
