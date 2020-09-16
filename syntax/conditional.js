afaeb3ff70c37b77f846697aa4faef22a30405dc
seunnnc <seunnnc@users.noreply.github.com>
2020-09-17 00:06:01 +0900

    js기본문법
    조건문 조건식

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,8 +1,12 @@
+var args = process.argv;
+console.log(args[2])
+
 console.log('a')
 console.log('b')
-if(false) {
+if(args[2] === '1') {
     console.log('c1')
 }else {
     console.log('c2')
 }
 console.log('d')
+//강의 19-1 시작해야함
