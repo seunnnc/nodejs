d12eed40d6c7b0a36eb58e8d0bc6deb9787fdabf
seunnnc <seunnnc@users.noreply.github.com>
2020-09-17 23:42:12 +0900

    node.js 기초

--- main.js
+++ main.js
@@ -5,15 +5,34 @@ var url = http.request('url')
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    console.log(queryData.id)
-    if(_url == '/'){
-      _url = '/index.html';
-    }
-    if(_url == '/favicon.ico'){
-        return response.writeHead(404);
-    }
-    response.writeHead(200)
-    response.end(queryData.id);
+    var title = queryData.id;
+
+    console.log(url.parse(_url, true))
+
+    fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description) {
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+      response.writeHead(200)
+      response.end(queryData.id);
+    })
+    
  
 });
 app.listen(3000);
